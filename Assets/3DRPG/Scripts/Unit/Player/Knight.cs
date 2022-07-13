1d9831a2cd2054ad85ef00814c496f909ad26603
Rased Jung <rased0727@gmail.com>
2022-07-13 15:38:45 +0900

    220713 1538

---
+++ Assets/3DRPG/Scripts/Unit/Player/Knight.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace RPG3D
+{
+    public class Knight : Player
+    {
+    }
+}
---
+++ Assets/3DRPG/Scripts/Unit/Player/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace RPG3D
+{
+    public class Player : Unit
+    {
+        void Update()
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+            {
+                // 공격!!             
+                Attack();
+            }
+        }
+    }
+}
